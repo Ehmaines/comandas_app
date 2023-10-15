64a7399012440bc7dca1f8719aad9d4485669e66
Eduardo Maines <edumaines27@gmail.com>
2023-10-15 19:44:04 -0300

    feat: adiciona telas de cadastro de Produtos, clientes e funcionários

--- src/mod_erro/erro.py
+++ src/mod_erro/erro.py
@@ -4,8 +4,8 @@ template_folder='templates')
 
 @bp_erro.app_errorhandler(404)
 def erro404(error):
-return render_template("form404.html", erroHttp=error)
+    return render_template("form404.html", erroHttp=error)
 
 @bp_erro.app_errorhandler(500)
 def erro500(error):
-return render_template("form500.html", erroHttp=error)
+    return render_template("form500.html", erroHttp=error)
